dc016f6f931c38bd206e4b1d4daa18671634544d
Jamwesayer <jamesb@live.nl>
2021-07-02 22:13:16 +0200

    updated comments

--- src/business/irepository.rs
+++ src/business/irepository.rs
@@ -1,24 +1,30 @@
 use crate::business::model::{PaymentRequest, GeneratedPaymentRequest, PaymentDetails};
 
 pub trait IPaymentRepository {
-    /// Returns a GeneratedPaymentRequest
+    /// Returns Result<GeneratedPaymentRequest, String>
+    ///
+    /// Creates a paymentwindow which will allow customers to pay and keep track of the payments using an unique identifier (transaction_id)
     ///
     /// # Arguments
     ///
     /// * `payment_request_model` - A PaymentRequest model
     fn create_payment_window(&self, payment_request_model: PaymentRequest) -> Result<GeneratedPaymentRequest, String>;
 
-    /// Returns PaymentDetails
+    /// Returns Result<PaymentDetails, String>
+    ///
+    /// Check the status of a certain payment window
     ///
     /// # Arguments
     ///
-    /// * `label` - A label which will be used to identify the address to check the payment status
+    /// * `label` - A &str which will be used to identify the payment window
     fn check_payment_status(&self, label: &str) -> Result<PaymentDetails, String>;
 
-    /// Returns
+    /// Returns Result<String, String>
+    ///
+    /// Refunds all the transactions done to
     ///
     /// # Arguments
     ///
-    /// * `label` - A label which will be used to identify the address to check the payment status
+    /// * `label` - A &str which will be used to identify the specific label for which the payments has been done
     fn refund(&self, label: &str) -> Result<String, String>;
 }
