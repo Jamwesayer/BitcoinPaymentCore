f324cfe7816d402288da57a70b12669b73171793
Jamwesayer <jamesb@live.nl>
2021-07-05 17:40:55 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,10 @@
-use tokio::time::sleep;
-
-use payment_core::data::idatasource::ITransactionNetworkDataSource;
-use payment_core::business::usecase::payment::PaymentUseCase;
-use payment_core::business::usecase::transaction::TransactionUseCase;
-use payment_core::data::idatasource::PaymentDatabase;
-use payment_core::data::idatasource::PaymentNetwork;
-use payment_core::data::idatasource::TransactionNetwork;
-use payment_core::presentation::item::PaymentRequestItem;
-use payment_core::data::repository::payment::PaymentRepository;
-use payment_core::data::repository::transaction::TransactionRepository;
-use payment_core::data::blockchain_util::import_address;
+use payment_core::presentation::controller_service::PaymentControllerService;
+use payment_core::presentation::item::*;
+use payment_core::data::database_util;
 
 #[tokio::main]
 async fn main() {
-    // let test = PaymentRequestItem::new("joost".to_string(), 0.001, 1);
-    let repo: PaymentRepository = Default::default();
-    // // let repo: PaymentRepository = PaymentRepository {
-    // //     payment_database_datasource: &PaymentDatabase::new(),
-    // //     payment_network_datasource: &PaymentNetwork::new()
-    // // };
-    // let usecase = PaymentUseCase::new(Box::new(repo));
-    // // println!("{:?}", usecase.create_payment_window(test));
-    // println!("{:?}", usecase.check_payment_status("joost"));
-    // match import_address("bcrt1q2mtlj023wx0k38wv5vjk4a6msukqgrxkgp6hmt") {
-    //     Ok(_) => println!("Created"),
-    //     Err(e) => println!("{}", e)
-    // }
 
-    let usecase = TransactionUseCase::default();
-    usecase.follow_transaction("testtesttest".to_string(), 10.0).await;
-    // let usecase = TransactionUseCase::newmain())
 
-    // transaction.follow_transactions_for_label("test12345", 2000.0);
-    // thread::sleep(Duration::from_millis(5000));
-    // transaction.follow_transactions_for_label("test", 1000.0);
-    // transaction.follow_transactions_for_label("test12345", 2000.0);
+    database_util::get_amount_of_transactions_for_shop(&1);
 }
