58219fe0d7a1f3c1bd38762f9b52cd8d1a720c02
Jamwesayer <jamesb@live.nl>
2021-07-05 16:58:08 +0200

    added transaction type

--- src/data/entity/transaction.rs
+++ src/data/entity/transaction.rs
@@ -7,16 +7,49 @@ pub struct TransactionEntity {
     amount: f64,
     transaction_id: String,
     origin_address: String,
+    transaction_type: i32,
     time_received: NaiveDateTime
 }
 
 impl TransactionEntity {
-    pub fn new(amount: f64, transaction_id: String, origin_address: String, time_received: NaiveDateTime) -> Self {
+    pub fn new(amount: f64, transaction_id: String, origin_address: String, transaction_type: i32, time_received: NaiveDateTime) -> Self {
         Self {
             amount: amount,
             transaction_id: transaction_id,
             origin_address: origin_address,
+            transaction_type: transaction_type,
             time_received: time_received
         }
+    }
+
+    pub fn get_amount(&self) -> &f64 {
+        &self.amount
+    }
+
+    pub fn get_transaction_id(&self) -> &str {
+        self.transaction_id.as_str()
+    }
+    pub fn get_time_received(&self) -> &NaiveDateTime {
+        &self.time_received
+    }
+    pub fn get_transaction_type(&self) -> &i32 {
+        &self.transaction_type
+    }
+    pub fn get_origin_address(&self) -> &str {
+        self.origin_address.as_str()
+    }
+
+    pub fn map_to_entity(model: Transaction) -> Self {
+        TransactionEntity {
+            amount: *model.get_amount(),
+            transaction_id: model.get_transaction_id().to_string(),
+            origin_address: model.get_origin_address().to_string(),
+            transaction_type: *model.get_transaction_type(),
+            time_received: *model.get_time_received()
+        }
+    }
+
+    pub fn map_to_business(&self) -> Transaction {
+        Transaction::new(*self.get_amount(), self.get_transaction_id().to_string(), self.get_origin_address().to_string(), *self.get_transaction_type(), *self.get_time_received())
     }
 }
